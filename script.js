7e2c0d2b93e4fedbdfddc4b5ccb3bce80f4f2992
HorrySheet11 <marinnathan11@gmail.com>
2025-10-10 19:21:50 +0800

    added reset button and functionality
    created reset button to reset div colors

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const section = document.createElement('section');
 section.classList.add('section');
 body.appendChild(section);
 
+const reset = document.createElement('button');
+reset.textContent = 'Reset';
+reset.classList.add('reset');
+body.appendChild(reset);
+
+reset.addEventListener('click', () => {
+    document.querySelectorAll('.box').forEach(box => {
+        box.style.backgroundColor = '#e3f4ff';
+    });
+});
+
+
 for (let i = 1; i <= 256  ; i++) {    
     const div = document.createElement('div');
     div.textContent = '';
@@ -15,6 +27,6 @@ document.querySelectorAll('.box').forEach(box => {
     box.addEventListener('mouseover', () => {
         box.style.backgroundColor = 'grey';
     });
-    box.style.cssText = "background: #e3f4ff; height: 10px; width: 10px; margin: 1px; padding: 8px;";
+    box.style.cssText = "background: #e3f4ff; height: 10px; width: 10px; margin: 0px; padding: 8px;";
 });
 
